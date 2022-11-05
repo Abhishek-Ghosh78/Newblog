b9972be75525276e9a102179342f7dece7689a04
Abhishek <abhishek@Abhisheks-MacBook-Air.local>
2022-11-06 03:32:44 +0530

    first commit

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -2,7 +2,7 @@
 
 import {GraphQLClient, gql} from 'graphql-request'
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS.ENDPOINT;
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
 export default async function comments(req, res) {
   // const { name, email, slug, comment} = req.body
